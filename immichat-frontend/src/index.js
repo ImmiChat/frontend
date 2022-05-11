f37fb9ea8f3a3b8d7af85c90f9628dd36bdf4037
raylu511 <raylu511@gmail.com>
2022-05-11 15:41:54 -0400

    Created authContext, provider and wrapped Provider around App component

--- immichat-frontend/src/index.js
+++ immichat-frontend/src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
+import AuthenticationContextProvider from './context/AuthenticationContextProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthenticationContextProvider>
+      <App />
+    </AuthenticationContextProvider>
   </React.StrictMode>
 );
 
