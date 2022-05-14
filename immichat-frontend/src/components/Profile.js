38b9ada5b6f77c86d48e2aecda264436bcd47d96
raylu511 <raylu511@gmail.com>
2022-05-14 18:37:24 -0400

    Finished profile component 1st draft

--- immichat-frontend/src/components/Profile.js
+++ immichat-frontend/src/components/Profile.js
@@ -1,9 +1,44 @@
 import React from "react";
+import { AccountCircle } from "@mui/icons-material";
 
 const Profile = (props) => {
+  const style = {
+    width: "90%",
+    height: "400px",
+    backgroundColor: "white",
+  };
+
+  const iconStyle = {
+    width: "150px",
+    height: "150px",
+  };
+
+  const imageStyle = {
+    backgroundImage:
+      "url(https://cdn.britannica.com/90/7490-004-BAD4AA72/Flag-China.jpg)",
+    backgroundSize: "80%",
+    width: "100%",
+    height: "50%",
+  };
+
   return (
-    <div>
-      <div></div>
+    <div
+      className="rounded border border-dark d-flex align-items-center flex-column"
+      style={style}
+    >
+      <div style={imageStyle} className="d-flex justify-content-center">
+        <AccountCircle className="pt-3" style={iconStyle} />
+      </div>
+
+      <h3>Ray Lu</h3>
+      <p>Country: China</p>
+      <p>Language: Chinese</p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus
+        exercitationem deserunt necessitatibus ipsum voluptates, ullam sunt quam
+        recusandae vel quas possimus ex natus esse aut accusamus in optio.
+        Totam, numquam.
+      </p>
     </div>
   );
 };
