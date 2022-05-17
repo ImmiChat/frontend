a555c3f3b5c00e72fb7c9f203e5f64890c2b63bc
Shawn Townsend <fr3sh9475@hotmail.com>
2022-05-17 16:00:44 -0400

    Finished profile page

--- immichat-frontend/src/components/ProfilePage.js
+++ immichat-frontend/src/components/ProfilePage.js
@@ -1,32 +1,29 @@
 import React from "react";
 import Navbar from "./Navbar";
-import AuthenticationContext from "../context/AuthenticationContext";
-// import { useState } from "react";
-import { useForm } from "react-hook-form";
+import { useState } from "react";
 import './profilepage.css'
 
 const ProfilePage = () => {
-  const { user, setUser } = React.useContext(AuthenticationContext);
-  const { register } = useForm();
-  // const [profilePageForm, setProfilePageForm] = useState({
-  //   firstName: "",
-  //   lastName: "",
-  //   username: "",
-  //   email: "",
-  //   country: "",
-  // });
+  const [profilePageForm, setProfilePageForm] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    language: "",
+    country: "",
+    bio: ""
+  });
 
   const handleProfilePageChange = (e) => {
     e.preventDefault();
-    setUser({
-      ...user,
+    setProfilePageForm({
+      ...profilePageForm,
       [e.target.name]: e.target.value,
     });
   };
   
   return (
     <div>
-      <Navbar/>
+    <Navbar/>
     <div className="container rounded bg-white mt-5 mb-5">
     <div className="row">
         <div className="col-md-3 border-right">
@@ -42,16 +39,29 @@ const ProfilePage = () => {
                 </div>
                 <div id="wrapper">
                 <div className="row mt-2">
-                    <div className="col-md-6"><label className="labels">First Name</label><input type="text" className="form-control" placeholder="First Name" value={user.first_name} onChange={handleProfilePageChange} ref={register({ required: true })}/></div>
-                    <div className="col-md-6"><label className="labels">Last Name</label><input type="text" className="form-control" value={user.last_name} onChange={handleProfilePageChange} placeholder="Last Name" ref={register({ required: true })}/></div>
+                    <div className="col-md-6">
+                      <label className="labels">First Name</label><input type="text" className="form-control" placeholder="First Name" defaultValue={profilePageForm.firstName}/>
+                      </div>
+                    <div className="col-md-6">
+                      <label className="labels">Last Name</label><input type="text" className="form-control" defaultValue={profilePageForm.lastName} onChange={handleProfilePageChange} placeholder="Last Name"/>
+                      </div>
                 </div>
                 <div className="row mt-3">
-                <div className="col-md-6"><label className="labels">Username</label><input type="text" className="form-control" value={user.user_name} onChange={handleProfilePageChange} placeholder="Username" ref={register({ required: true })}/></div>
-                    <div className="col-md-12"><label className="labels">Email</label><input type="text" className="form-control" placeholder="Email" value={user.email} onChange={handleProfilePageChange} ref={register({ required: true })}/></div>
+                    <div className="col-md-12"><label className="labels">Email</label>
+                    <input type="email" className="form-control" placeholder="Email" defaultValue={profilePageForm.email} onChange={handleProfilePageChange}/>
+                    </div>
+                    <div className="col-md-6"><label className="labels">Language</label>
+                    <input type="text" className="form-control" defaultValue={profilePageForm.language} onChange={handleProfilePageChange} placeholder="Language"/>
+                    </div>
                 </div>
                 <div className="row mt-3">
-                    <div className="col-md-6"><label className="labels">Country of Origin</label><input type="text" className="form-control" placeholder="Country" value={user.country} onChange={handleProfilePageChange} ref={register({ required: true })}/></div>
+                    <div className="col-md-6"><label className="labels">Country of Origin</label>
+                    <input type="text" className="form-control" placeholder="Country" defaultValue={profilePageForm.country} onChange={handleProfilePageChange}/>
+                    </div>
                 </div>
+                <div className="col-md-12"><label className="labels">Bio</label>
+                    <input type="text" className="form-control" placeholder="Bio" defaultValue={profilePageForm.bio} onChange={handleProfilePageChange}/>
+                </div>    
                 <div className="mt-5 text-center"><button className="btn btn-primary profile-button" type="button">Save Profile</button></div>
                 </div>
             </div>
