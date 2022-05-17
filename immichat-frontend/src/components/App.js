a1c35da0ac39e2ba9082bf6d29ac6abecf642290
raylu511 <raylu511@gmail.com>
2022-05-17 14:35:34 -0400

    Deleted unneccessary files

--- immichat-frontend/src/components/App.js
+++ immichat-frontend/src/components/App.js
@@ -1,7 +1,5 @@
 import "./App.css";
 import FormComponent from "./formStuff";
-import Home from "./Home";
-import ProfilePage from "./ProfilePage";
 import Error from "./Error";
 import HomePage from './HomePage';
 import React from "react";
@@ -11,9 +9,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      {/* <Route path="/home" element={<Home />} />
-      <Route path="/profile" element={<ProfilePage />} />
-      <Route path="*" element={<Error />} /> */}
+      <Route path="*" element={<Error />} />
     </Routes>
   );
 }
