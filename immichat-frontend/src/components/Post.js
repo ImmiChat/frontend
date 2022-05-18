4151d3b9c6a3ef3181f301fd5c257f0e9fdc4cbf
raylu511 <raylu511@gmail.com>
2022-05-18 12:58:12 -0400

    Conditional rendering done

--- immichat-frontend/src/components/Post.js
+++ immichat-frontend/src/components/Post.js
@@ -1,14 +1,73 @@
-import { AccountCircle, ThumbUpAlt, Comment } from "@mui/icons-material";
+import {
+  AccountCircle,
+  ThumbUpAlt,
+  Comment,
+  Edit,
+  Delete,
+} from "@mui/icons-material";
+import FeedContext from "../context/FeedContext";
 import convertTimeStamp from "../utils/convertTimeStamp";
+import React from "react";
+import AuthenticationContext from "../context/AuthenticationContext";
 
 const Post = ({ info }) => {
+
+  const { feed, setFeed } = React.useContext(FeedContext);
+  const { user } = React.useContext(AuthenticationContext);
+  const handleDelete = (event) => {
+    async function deletePost(id) {
+        const response = await fetch(`http://localhost:9000/posts/${id}`,{
+          method: 'DELETE',
+          headers: {
+            'content-type': 'application/json'
+          }
+        });
+        const data = await response.json();
+        const newFeed = feed.filter(post => post.id !==  parseInt(event.target.id));
+        setFeed(newFeed);
+    }
+    deletePost(event.target.id);
+  }
   return (
-    <div className="border rounded border-dark px-3 my-5">
-      <div className="d-flex align-items-center">
-        <AccountCircle style={{ width: "50px", height: "75px" }} />
-        <span className="fw-bold px-2">
-          {info.first_name} {info.last_name}
-        </span>
+    <div className="border rounded border-dark px-3 my-5" id={info.id}>
+      <div className="d-flex justify-content-between align-items-center">
+        <div className="d-flex align-items-center">
+          <AccountCircle style={{ width: "50px", height: "75px" }} />
+          <span className="fw-bold px-2">
+            {info.first_name} {info.last_name}
+          </span>
+        </div>
+        {user.id === info.user_id && (
+          <div className="dropdown">
+            <button
+              className="btn"
+              type="button"
+              id="dropdownMenuButton1"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              ...
+            </button>
+
+            <ul
+              className="dropdown-menu list"
+              aria-labelledby="dropdownMenuButton1"
+            >
+              <li className="">
+                <button className="btn edit" id={info.id}>
+                  <Edit className="mx-2 " />
+                  Edit
+                </button>
+              </li>
+              <li>
+                <button onClick={handleDelete} className="btn delete" id={info.id}>
+                  <Delete className="mx-2" />
+                  Delete
+                </button>
+              </li>
+            </ul>
+          </div>
+        )}
       </div>
       <p className="px-1">{info.body}</p>
       <p className="px-1 text-secondary small">
