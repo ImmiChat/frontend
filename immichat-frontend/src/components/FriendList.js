2f1b1797baaafaed954fe0d5d0a4b391bccf9e0e
Shawn Townsend <fr3sh9475@hotmail.com>
2022-05-19 23:49:30 -0400

    Restyled search bar and restyled friends list

--- immichat-frontend/src/components/FriendList.js
+++ immichat-frontend/src/components/FriendList.js
@@ -1,56 +1,104 @@
-import { AccountCircle } from "@mui/icons-material";
+import { AccountCircle } from '@mui/icons-material';
+import './FriendList.css';
 
 const FriendList = () => {
-  return (
-    <ul className="p-0 list-group">
-      <a href="">
-        <li className="list-group-item d-flex justify-content-between">
-          <div>
-            <AccountCircle style={{ width: "50px", height: "50px" }} />
-            <span>Ray</span>
+  function handeReload(e){
+    e.preventDefault();
+  }
+  return(
+    <div className="container">
+    <div className="row">
+      <div className="col-md-8">
+        <div className="people-nearby">
+          <div className="nearby-user">
+            <div className="row">
+              <div className="col-md-2 col-sm-2">
+                <AccountCircle style={{ width: '50px', height: '50px' }} />
+              </div>
+              <div className="col-md-7 col-sm-7">
+                <h5 className="names">
+                  {/* <a href="#" className="profile-link"> */}
+                  Sophia Page
+                {/* </a> */}
+                </h5>
+              </div>
+              <div className="friendBtn col-md-3 col-sm-3" onClick={handeReload}>
+                <a href="/">
+                  <button className="bn54">
+                      <span className="bn54span">Unfriend</span>
+                    </button>
+                </a>
+              </div>
+            </div>
           </div>
-          <button className="btn btn-secondary rounded-pill px-4">Unfriend</button>
-        </li>
-      </a>
-      <a href="">
-        <li className="list-group-item d-flex justify-content-between">
-          <div>
-            <AccountCircle style={{ width: "50px", height: "50px" }} />
-            <span>Ray</span>
+          <div className="nearby-user">
+            <div className="row">
+              <div className="col-md-2 col-sm-2">
+                <AccountCircle style={{ width: '50px', height: '50px' }} />
+              </div>
+              <div className="col-md-7 col-sm-7">
+                <h5 className="names">
+                  {/* <a href="#" className="profile-link"> */}
+                  Emma Johnson
+                {/* </a> */}
+                </h5>
+              </div>
+              <div className="friendBtn col-md-3 col-sm-3" onClick={handeReload}>
+                <a href="/">
+                  <button className="bn54">
+                      <span className="bn54span">Unfriend</span>
+                    </button>
+                </a>
+              </div>
+            </div>
           </div>
-          <button className="btn btn-secondary rounded-pill px-4">Unfriend</button>
-        </li>
-      </a>
-      <a href="">
-        <li className="list-group-item d-flex justify-content-between">
-          <div>
-            <AccountCircle style={{ width: "50px", height: "50px" }} />
-            <span>Ray</span>
+          <div className="nearby-user">
+            <div className="row">
+              <div className="col-md-2 col-sm-2">
+                <AccountCircle style={{ width: '50px', height: '50px' }} />
+              </div>
+              <div className="col-md-7 col-sm-7">
+                <h5 className="names">
+                  {/* <a href="#" className="profile-link"> */}
+                  Nora Wilson
+                {/* </a> */}
+                </h5>
+              </div>
+              <div className="friendBtn col-md-3 col-sm-3" onClick={handeReload}>
+                <a href="/">
+                  <button className="bn54">
+                      <span className="bn54span">Unfriend</span>
+                    </button>
+                </a>
+              </div>
+            </div>
           </div>
-          <button className="btn btn-secondary rounded-pill px-4">Unfriend</button>
-        </li>
-      </a>
-      <a href="">
-        <li className="list-group-item d-flex justify-content-between">
-          <div>
-            <AccountCircle style={{ width: "50px", height: "50px" }} />
-            <span>Ray</span>
+          <div className="nearby-user">
+            <div className="row">
+              <div className="col-md-2 col-sm-2">
+                <AccountCircle style={{ width: '50px', height: '50px' }} />
+              </div>
+              <div className="col-md-7 col-sm-7">
+                <h5 className="names">
+                  {/* <a href="#" className="profile-link"> */}
+                  Diana Amber
+                {/* </a> */}
+                </h5>
+              </div>
+              <div className="friendBtn col-md-3 col-sm-3" onClick={handeReload}>
+                <a href="/">
+                  <button className="bn54">
+                      <span className="bn54span">Unfriend</span>
+                    </button>
+                </a>
+              </div>
+            </div>
           </div>
-          <button className="btn btn-secondary rounded-pill px-4">Unfriend</button>
-        </li>
-      </a>
-      <a href="">
-        <li className="list-group-item d-flex justify-content-between">
-          <div>
-            <AccountCircle style={{ width: "50px", height: "50px" }} />
-            <span>Ray</span>
-          </div>
-          <button className="btn btn-secondary rounded-pill px-4">Unfriend</button>
-        </li>
-      </a>
-     
-    </ul>
-  );
+        </div>
+      </div>
+    </div>
+  </div>
+  )
 };
 
 export default FriendList;
